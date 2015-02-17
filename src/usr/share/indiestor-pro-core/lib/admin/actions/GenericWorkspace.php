9e705e1c881a6b040a06835a8a6eec88bc0d4faa
Erik Poupaert <erik@sankuru.biz>
2015-02-17 21:36:23 +0700

    workspace/add create folder or zfs dataset

--- src/usr/share/indiestor-pro-core/lib/admin/actions/GenericWorkspace.php
+++ src/usr/share/indiestor-pro-core/lib/admin/actions/GenericWorkspace.php
@@ -16,6 +16,13 @@ class GenericWorkspace extends EntityType
                 $conf=new EtcWorkspaces('generic');
 		$workspace=ProgramActions::$entityName;
                 $path=$commandAction->actionArg;
+                if(substr($path,0,1)!=='/')
+                        $pathAbs="/$path";
+                else $pathAbs=$path;
+                $fileSystem=sysquery_df_filesystem_for_folder(dirname($pathAbs));
+		if($fileSystem=='zfs')
+                        ShellCommand::exec_fail_if_error("zfs create $path");
+                else mkdir($path);
                 $conf->add($workspace,$path);
                 $conf->save();
         }
